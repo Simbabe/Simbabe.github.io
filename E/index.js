b4546a55d1954258dc56746d4f9767eeededd016
Simbabe <106524504+Simbabe@users.noreply.github.com>
2023-05-19 04:17:04 -0700

    Add files via upload

---
+++ E/index.js
@@ -0,0 +1,36 @@
+//document.getElementById("count-el").innerText =5
+// let count= 5+7
+
+// console.log(count)
+
+// let age=30;
+// console.log(age)
+
+// let firstBatch =5
+// let selectedColor =null
+// let name= 'zayn';
+// let Age = 60;
+// let isApproved = false;
+// let firstName = undefined;
+// let stakeHolders = null;
+// let person ={
+// name:'mosh'
+// age: 30
+// };
+  
+
+// Array //
+// let selectedColors = ['red' ,'green'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+// fuctions//
+
+// function greet(name, lastName) {
+//     console.log('hello ' + name + ''+ lastName );
+//  greet('john');
+// greet('mary');
+// m                                                                                         
+
+
+}
